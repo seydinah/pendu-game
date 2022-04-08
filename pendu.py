30700a27cb3c62fde5b448462c9b4e6747025bb9
seydinah <seydinah94@gmail.com>
2022-04-08 02:42:34 +0000

    updated

--- pendu.py
+++ pendu.py
@@ -7,28 +7,63 @@ Ce jeu est basé sur un concept consistant à deviner les bonnes lettres pour fo
 """
 
 import random
-from dico import dico # importer la liste de plusieurs mots
+from dico import dico
 
-def presentation():
-    """Ci-après, un affichage juste pour le fun"""
-    joueur =input("Bonjour, quel est votre nom ? >>> ")
 
-    print("*****************************")
-    print("*      Jeu de pendu         *")
-    print(f"*   Bienvevue {joueur}\U0001F637      *")
-    print("*                           *")
-    print("*****************************")
-    print()
+def welcome():
+    
+    joueur = input("""
+                ========================
+                > Quel est votre nom:  <
+                """).capitalize()
+    
+    if joueur.isalpha() == True:
+        print("*****************************")
+        print("*      Jeu de pendu         *")
+        print(f"*   Bienvevue {joueur}\U0001F637      *")
+        print("*                           *")
+        print("*****************************")
+        print()
+        
+           
+    else:
+        print('Svp, utilisez des carractères !')
+        joueur = input("Saisir un nom:  ")
+        print('Bonjour !',joueur,'Bienvenue au jeu de pendu !')
+        
+def play_again():
+    
+    """ La fonction demande au joueur s'il souhaite rejouer ou non """
+    reponse = input("Voulez vous rejouer à nouveau? oui/non. Tapez 'O' pour Oui or 'N' pour Non: ").lower()
 
-def recuperer_mot():
+    if reponse == "o" or reponse == "O":
+        jouer()
+    else:
+        print("A plutard !!")
+        
+def get_word():
     """
     Là on définit une fonction qui renvoie un mot aléatoire d'une liste de plusieurs mots
     """
-    word = random.choice(dico)
-    return word.upper()
+    words = random.choice(dico)
+    return words.lower()
 
+def tirets(word):
+    print("Le mot contient", len(word), "lettres.")
+    print(len(word) * '_')
 
-def jouer(word):
+def statut(tentatives,word,status):
+    if status == word:
+        print("Great Job! Vous avez deviné le mot correctement!")
+        devine = True
+    elif tentatives == 0:
+        print("Oupps! Vous avez perdu!")
+
+def traitement(proposition,alphabet):
+    if proposition not in alphabet:
+        print("Verifiez votre saisi ")
+    
+def jouer():
     """
     fonction déclenchant une partie du jeu :
      * Affichage du mot à deviner
@@ -36,67 +71,64 @@ def jouer(word):
      * Mise à jour de l'affichage
      On répète cela jusqu'a ce que le mot complet soit trouvé / le nombre de tentative soit terminé
     """
-    affichage = "_" * len(word)
-    deviné = False
-    lettre_deviné = []
-    mot_deviné = []
-    tentatives = 10
-    print("Essayez de deviner le mot caché !")
-    print(affichage)
-    print("\n")
-    while not deviné and tentatives > 0:
-        joueur = input("Proposez une lettre: ").upper()
-        if len(joueur) == 1 and joueur.isalpha():
-            if joueur in lettre_deviné:
-                print("Vous avez deja deviné la lettre", joueur)
-            elif joueur not in word:
-                print(f"Le caractère {joueur} n'est pas dans le mot.")
-                tentatives -= 1
-                lettre_deviné.append(joueur)
+    word = get_word()
+    devine_lettre = []
+    tentatives = 7
+    devine = False
+    alphabet = ("abcdefghijklmnopqrstuvwxyz")
+    print()
+    tirets(word)
+    
+    while devine == False and tentatives > 0:
+        print("Vous avez " + str(tentatives) + " tentatives")
+        proposition = input("Proposez une lettre svp >>> ").lower()
+        if len(proposition) == 1:
+            traitement(proposition,alphabet)
+            if proposition in devine_lettre:
+                print("Vous avez dèja saisi cette lettre! Essayez encore")
+            elif proposition not in word:
+                print("Desolé ! la lettre n'est pas dans le mot! ")
+                devine_lettre.append(proposition)
+                tentatives -=1
+            elif proposition in word:
+                print("Bien vu!")
+                devine_lettre.append(proposition)
             else:
-                print(f"Bien joué! {joueur} est dans le mot!")
-                lettre_deviné.append(joueur)
-                liste_mot = list(affichage)
-                indices = [i for i, letter in enumerate(word) if letter == joueur]
-                for index in indices:
-                    liste_mot[index] = joueur
-                affichage = "".join(liste_mot)
-                if "_" not in affichage:
-                    deviné = True
-        elif len(joueur) == len(word) and joueur.isalpha():
-            if joueur in mot_deviné:
-                print(f"Vous avez deja deviné le mot {joueur}")
-            elif joueur != word:
-                print(f"{joueur} n'est pas dans le mot.")
-                """ Si la lettre proposé n'est pas dans le mot {tentatives} dimunie de 1 """
-                tentatives -= 1
-                mot_deviné.append(joueur)
-            else:
-                deviné = True
-                affichage = word
-        else:
-            print("Mauvaise proposition.")
-        print(f"Il vous reste {tentatives} tentatives")
-        print("\n")
-        print(affichage)
-        print("\n")
-    if deviné:
-        print(f"Bravo, vous avez deviné {word}! Vous avez gagné!")
-    else:
-        print(f"Désolé, vous avez perdu. Le mot était {word}. Une prochaine fois!")
+                print("Mauvais saisi! ")
 
+        elif len(proposition) == len(word):
+            if proposition == word:
+                print("Bien joué! ")
+                devine = True
+            else:
+                print("Désolé, ce n'etait pas le mot")
+                tentatives -= 1
+        else:
+            print("La longueur du mot n'est pas le même.")
+            tentatives -=1
+        
+        if devine == False:
+            status=""
+            for lettre in word:
+                if lettre in devine_lettre:
+                    status += lettre
+                else:
+                    status += "_"
+            print(status)
+        if status == word:
+            print("Great Job! Vous avez deviné le mot correctement!")
+            devine = True
+        if tentatives == 0:
+            print("Oupps! Vous avez perdu!")
+
+    play_again()
 
 def main():
     """
     fonction principale du jeu, servant de point d'entrée au programme, elle permet aussi d'effectuer plusieurs manches
     """
-    presentation()
-    word = recuperer_mot()
-    jouer(word)
-    while input("Voulez-vouz rejouer ? (O/N) ").upper() == "O":
-        word = recuperer_mot()
-        jouer(word)
+    welcome()
+    jouer()
 
-
-if __name__ == "__main__":
+if __name__ =="__main__":
     main()
